d0d6c3376ccb7c013649ba0bba02767089ea412e
elegant-moon <elegantmoon.tech@gmail.com>
2020-04-23 10:30:39 +0200

    Update deploy

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
 var config = require("./webpack/dev");
 
+const PORT = process.env.PORT || 3030;
+
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   inline: false,
   historyApiFallback: true,
   quiet: true,
-}).listen(3030, "localhost", function (error, result) {
+}).listen(PORT, "localhost", function (error, result) {
   if (error) {
     console.log(error);
   }
